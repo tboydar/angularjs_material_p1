4f4880e2dd474efe99f42f0888fdd911c255dd62
DAR-T460\tboydar <tboydar@gmail.com>
2017-09-22 14:05:23 +0800

    https://material.angular.io/guide/getting-started   add https://material.angular.io/components/progress-spinner/overview

--- ngnew/src/app/app.module.ts
+++ ngnew/src/app/app.module.ts
@@ -3,12 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule, MdCheckboxModule, MatProgressSpinnerModule} from '@angular/material';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
